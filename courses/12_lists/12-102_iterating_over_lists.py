669a8fe748046438d4fe5e04607c8abe9d047c9c
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-05 15:31:35 +0800

    12-102: Iterating over lists

---
+++ courses/12_lists/12-102_iterating_over_lists.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4]
+
+for number in numbers:
+    print(number)
+
+colors = ["purple", "teal", "magenta", "crimson", "emerald"]
+index = 0
+
+while index < len(colors):
+    print(colors[index])
+    index += 1
