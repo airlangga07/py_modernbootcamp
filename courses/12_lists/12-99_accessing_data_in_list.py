4d5e79d7b160764be4a99f01507edcccfc39a4a1
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-05 15:07:13 +0800

    12-99: Accessing data in list

---
+++ courses/12_lists/12-99_accessing_data_in_list.py
@@ -0,0 +1,13 @@
+colors = ["Teal", "Purple", "Orange"]
+
+# accessing list from the first
+print(colors[0])
+print(colors[1])
+print(colors[2])
+
+# accessing list from the back
+print(colors[-1])
+print(colors[-2])
+
+# check value is in a list
+print("Teal" in colors)
