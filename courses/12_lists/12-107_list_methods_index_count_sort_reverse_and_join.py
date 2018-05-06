cb5f40a638465aa443e7dcc2f4d7fc0cf0c70e27
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-06 12:57:19 +0800

    12-107: List methods: count, sort, reverse and join

---
+++ courses/12_lists/12-107_list_methods_index_count_sort_reverse_and_join.py
@@ -0,0 +1,22 @@
+# index finding the position of a value in a list
+nums = [6, 7, 8, 9, 10]
+numbers.index(6) # 1
+numbers.index(9) # 4
+
+# count return the number of times x appears in the list
+nums = [1, 1, 2, 3, 4, 5, 5]
+nums.count(1) # 2
+
+# reverse will reverse elements in the list (in-place)
+nums = [1, 2, 3, 4]
+nums.reverse()
+print(nums) # [4, 3, 2, 1]
+
+# sort will sort the item of the list
+nums = [5, 2, 4, 1, 3]
+nums.sort()
+print(nums) # [1, 2, 3, 4, 5]
+
+# join will combine every item in the list with the specified string
+words = ['Coding', 'is', 'fun']
+print(' '.join(words))
