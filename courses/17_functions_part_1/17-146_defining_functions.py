407da624b0f3de5ab3c0e4f27cdd1c7e27a15995
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-08 21:52:30 +0800

    17-146: Defining functions

---
+++ courses/17_functions_part_1/17-146_defining_functions.py
@@ -0,0 +1,8 @@
+# our own functions
+def sing_happy_birthday():
+    print("Happy Birthday to You")
+    print("Happy Birthday to You")
+    print("Happy Birthday to Dear You")
+    print("Happy Birthday to You")
+
+sing_happy_birthday()
