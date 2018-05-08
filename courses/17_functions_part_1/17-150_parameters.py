9f4a372eba15d69d1be520e874822a7e4a7a04e0
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-08 22:17:33 +0800

    17-150: Parameters

---
+++ courses/17_functions_part_1/17-150_parameters.py
@@ -0,0 +1,18 @@
+def square(num):
+    return num * num
+
+print(square(4))
+print(square(8))
+
+def sing_happy_birthday(name):
+    print("Happy Birthday to You")
+    print("Happy Birthday to You")
+    print("Happy Birthday Dear {}".format(name))
+    print("Happy Birthday to You")
+
+sing_happy_birthday("Mikael")
+
+def divide(num1, num2):
+    return num1 / num2
+
+print(divide(2, 1))
