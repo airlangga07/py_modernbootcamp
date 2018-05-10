bbb607ffc6eecda96e362df884cbb36da4529bba
Mikael Airlangga Trianggoro - RPi <mikael.airlangga@gmail.com>
2018-05-10 22:55:02 +0800

    20-198: abs, sum, and round

---
+++ courses/20_lambdas_and_builtin_functions/20-198_abs_sum_round.py
@@ -0,0 +1,13 @@
+# abs returns the absolute value of a numbero
+abs(-4) # 4
+abs(4) # 4
+
+# sum takes an iterable and an optional start
+# returns the sum of a start and the iters of an iterable from left to right
+# and returns the total
+sum([1, 2, 3, 4, 5]) # 15
+sum([1, 2, 3, 4, 5], 10) # 25
+
+# round will rounded to ndigits precision
+round(10.2) # 10
+round(1.21212121, 2) # 1.21
