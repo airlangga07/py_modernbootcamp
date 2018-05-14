bbc87819ed76f3a0e8ac95622127c8647b284ee7
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-05-14 22:09:41 +0800

    23-230: API PRoject

---
+++ courses/23_http_request/23-230_API_project.py
@@ -0,0 +1,26 @@
+from random import choice
+import requests
+
+term = input("What would you like to search for? ")
+url = "https://icanhazdadjoke.com/search"
+
+response = requests.get(
+    url, 
+    headers={ "Accept": "application/json" },
+    params={ "term": term }
+).json()
+
+# get the total number of jokes
+num_jokes = response['total_jokes']
+# get the results
+results = response['results']
+
+if num_jokes > 1:
+    print("I found {} jokes about {}. Here's one: ".format(num_jokes, term))
+    print(choice(results)['joke'])
+elif num_jokes == 1:
+    print("I found one joke about {}, here it is: ".format(term))
+    print(results[0]['joke'])
+else:
+    print("Sorry, couldn't find a joke with your term: {}".format(term))
+    
