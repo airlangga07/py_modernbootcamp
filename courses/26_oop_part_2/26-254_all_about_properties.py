f519155092f4a007bac2066238777a31c90d7a33
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-05-16 23:11:25 +0800

    26-254: All About Properties

---
+++ courses/26_oop_part_2/26-254_all_about_properties.py
@@ -0,0 +1,34 @@
+class Human:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        if age >= 0:
+            self._age = age
+        else:
+            self._age = 0
+    
+    # getter
+    @property
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+
+    # getter
+    @property
+    def age(self):
+        return self._age
+    
+    # setter
+    @age.setter
+    def age(self, value):
+        if value >= 0:
+            self._age = value
+        else:
+            raise ValueError("Age cannot be negative!")
+    
+
+
+jane = Human("Jane", "Goddall", 50)
+print(jane.age)
+jane.age = 20
+print(jane.age)
+print(jane.full_name)
