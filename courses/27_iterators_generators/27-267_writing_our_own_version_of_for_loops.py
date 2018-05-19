4595ccb8dae8116e93486e230e7c9b11c50f3456
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-05-19 22:31:04 +0800

    27-267: Writing our own for loops

---
+++ courses/27_iterators_generators/27-267_writing_our_own_version_of_for_loops.py
@@ -0,0 +1,18 @@
+# custom for loop
+def my_for(iterable, func):
+    iterator = iter(iterable)
+    while True:
+        try:
+            thing = next(iterator)
+        except StopIteration:
+            print("end of iterator")
+            break
+        else:
+            func(thing)
+        
+def square(x):
+    print(x * x)
+
+my_for('hello', print)
+my_for([1, 2, 3, 4, 5], print)
+my_for([1, 2, 3, 4, 5], square)
