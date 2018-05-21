82aca4c6d11d13909d04ed718abfb2c8a92f6db7
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-05-22 00:04:23 +0800

    29-296: doctests

---
+++ courses/29_testing_with_python/29-296_doctests.py
@@ -0,0 +1,25 @@
+# you can run this in the terminal: 
+# python -m doctest -v doctest.py
+def add(a, b):
+    """
+    >>> add(2, 3)
+    5
+    >>> add(100, 200)
+    300
+    """
+    return a + b
+
+def double(values):
+    """ Double the values in a list
+    >>> double([1, 2, 3, 4])
+    [2, 4, 6, 8]
+    >>> double([])
+    []
+    >>> double(['a', 'b', 'c'])
+    ['aa', 'bb', 'cc']
+    >>> double([True, None])
+    Traceback (most recent call last):
+        ...
+    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+    """
+    return [ 2 * elem for elem in values]
