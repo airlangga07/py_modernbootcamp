f1c1b3ca79d4d90efc4ef03ff483cc471fafea56
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-05-21 23:15:14 +0800

    28-291: enforcing argument types with a decorator

---
+++ courses/28_decorators/28-291_enforcing_argument_types_with_a_decorator.py
@@ -0,0 +1,17 @@
+def enforce(*types):
+    def decorator(f):
+        def new_func(*args, *kwargs):
+            # confert args into something mutable
+            newargs = []
+            for (a, t) in zip(args, types):
+                newargs.append(t(a))
+            return f(*newargs, **kwargs)
+        return new_func
+    return decorator
+
+@enforce(str, int)
+def repeat_msg(msg, times):
+    for time in range(times):
+        print(msg)
+
+repeat_msg('hello', '3')
